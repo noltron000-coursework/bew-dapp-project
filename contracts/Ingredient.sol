2849134cf9998a2383c04de05648b42a9b12538c
Nolan Kovacik <noltron000@gmail.com>
2019-06-25 12:52:09 -0700

    popped some stashed changes

--- contracts/Ingredient.sol
+++
@@ -1,9 +0,0 @@
-pragma solidity ^0.5.8;
-
-import "node_modules/erc721x/contracts/Core/ERC721X/ERC721XToken.sol";
-import "node_modules/erc721x/contracts/Libraries/ObjectsLib.sol";
-
-// Common Items
-contract Ingredient is ERC721XToken, Ownable {
-  //
-}
