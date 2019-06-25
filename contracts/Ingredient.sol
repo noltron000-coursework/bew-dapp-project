f06eecb64932c32b1e56b661e4e28309279ed733
EconClass <bizmail327@gmail.com>
2019-06-25 15:36:57 -0700

    added minting and supply check

--- contracts/Ingredient.sol
+++ contracts/Ingredient.sol
@@ -3,7 +3,25 @@ pragma solidity ^0.5.8;
 import "node_modules/erc721x/contracts/Core/ERC721X/ERC721XToken.sol";
 import "node_modules/erc721x/contracts/Libraries/ObjectsLib.sol";
 
-// Common Items
+/**
+ * @title Common tier sandwich ingredients
+ */
 contract Ingredient is ERC721XToken, Ownable {
-  //
+
+  // constructor (string memory name, string memory symbol) public {
+  //   name = "Common";
+  //   symbol = "COM";
+  // }
+  
+  mapping (uint=>uint8) tokenToIndividualSupply;
+  
+  function individualSupply(uint _tokenId) {
+    return tokenToIndividualSupply[_tokenId];
+  }
+
+  function mintToken(uint _tokenId, uint8 _supply) public onlyOwner {
+    require(!exists(_tokenId), "Error: Token already exists");
+    _mint(_tokenId, msg.sender, _supply);
+    tokenIdToIndividualSupply[_tokenId] = _supply;
+  }
 }
