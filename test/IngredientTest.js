3165d109202207fbe77fadfce2e830c598b6a635
Nolan Kovacik <noltron000@gmail.com>
2019-06-25 11:38:26 -0700

    adding more tests
    using dani's rainbow coins as a base reference: https://github.com/droxey/rainbowcoin

--- test/IngredientTest.js
+++ test/IngredientTest.js
@@ -32,38 +32,86 @@ contract('Ingredient', async function (accounts) {
       assert(true === true, 'this is true');
     });
 
-    it("Should make first account an owner", async () => {
+    it('Should make first account an owner', async () => {
+      // TODO:
+      // - What does deployed() do?
+      // - Where is from?
       let instance = await Ingredient.deployed();
       let owner = await instance.owner();
       assert.equal(owner, accounts[0]);
     });
 
-    it("Should create 1 slice of bread.", async () => {
+    it('Should start with no existing tokens', async () => {
+      let instance = await Ingredient.deployed();
+      // Begin with zero balance
+      let zeroBalance = await instance.totalSupply();
+      assert(
+        // TODO:
+        // - What is toString()?
+        // - Why does it take in a parameter of 10?
+        zeroBalance.toString(10) === '0',
+        'Contract should have no tokens at this point'
+      )
+    });
+
+    it('Should bake 1 slice of bread when asked', async () => {
+      let instance = await Ingredient.deployed();
+      // Try minting (baking) a new token
+      try {
+        const address = '';
+        const tokenId = '';
+        await instance.mint(address, tokenId);
+        // TODO:
+        // - What parameters would this take?
+        // - Where do they come from?
+      } catch (error) {
+        console.log(error);
+        assert(false, error);
+      }
+
+      // Ensure totalSupply is increased to 1
+      let totalSupply = await instance.totalSupply();
+      assert(
+        totalSupply.toString(10) === '1',
+        'Contract should have balance of 1 instead it has: ' + totalSupply.toString(10)
+      );
+
+      // make sure the token at index 0 has id 1
+      let tokenId = await instance.tokenOfOwnerByIndex(accounts[0], '0');
+      assert(
+        tokenId.toString(10) === '1',
+        'Token at index 0 is ' + tokenId.toString(10) // TODO: why 10?
+      )
+
+    });
+
+    it("Should mint an item to the owner only", async () => {
+      let instance = await Ingredient.deployed();
+      let other = accounts[1];
+      const address = '';
+      const tokenId = '';
+
+      await instance.transferOwnership(other);
+      await assertRevert(instance.mint(address, tokenId));
+    });
+
+    it('Should mint 24 slices of bread to the deli; for a bag of bread', async () => {
       let instance = await Ingredient.deployed();
       // TODO: Write the code here to call a contract function
     });
 
-    it("Should have bread owned by the bakery.", async () => {
-      let instance = await Ingredient.deployed();
-      // TODO: Write the code here to call a contract function
-
-    it("Should mint 24 slices of bread; for a bag of bread.", async () => {
+    it('Should trade 1 slice of bread for another slice of bread properly', async () => {
       let instance = await Ingredient.deployed();
       // TODO: Write the code here to call a contract function
     });
 
-    it("Should trade 1 slice of bread for another slice of bread properly.", async () => {
-      let instance = await Ingredient.deployed();
-      // TODO: Write the code here to call a contract function
-    });
-
-    it("Should trade 1 slice of bread for nothing correctly.", async () => {
+    it('Should trade 1 slice of bread for nothing correctly', async () => {
       let instance = await Ingredient.deployed();
       // TODO: Write the code here to call a contract function
     });
 
 /*
-    it("Should call another function on your deployed contract", async () => {
+    it('Should call another function on your deployed contract', async () => {
       let instance = await Ingredient.deployed();
       // TODO: Write the code here to call a contract function
     });
