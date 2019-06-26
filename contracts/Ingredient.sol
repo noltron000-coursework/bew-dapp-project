6b8ff3cdb12d8e544b4a17eb89945243b06cd395
EconClass <bizmail327@gmail.com>
2019-06-26 13:26:38 -0700

    [add] transfer token feature

--- contracts/Ingredient.sol
+++ contracts/Ingredient.sol
@@ -7,13 +7,18 @@ import "node_modules/erc721x/contracts/Libraries/ObjectsLib.sol";
  * @title Common tier sandwich ingredients
  */
 contract Ingredient is ERC721XToken, Ownable {
-
-  // constructor (string memory name, string memory symbol) public {
-  //   name = "Common";
-  //   symbol = "COM";
-  // }
   
   mapping (uint=>uint8) tokenToIndividualSupply;
+
+  event TokenAwarded(uint indexed tokenId, address claimer, uint8 amount);
+
+  function name() returns(string) {
+    return "Some Ingredient";
+  }
+
+  function symbol() returns(string) {
+    return "ING";
+  }
   
   function individualSupply(uint _tokenId) {
     return tokenToIndividualSupply[_tokenId];
@@ -23,5 +28,17 @@ contract Ingredient is ERC721XToken, Ownable {
     require(!exists(_tokenId), "Error: Token already exists");
     _mint(_tokenId, msg.sender, _supply);
     tokenIdToIndividualSupply[_tokenId] = _supply;
+  }
+
+  function awardToken(uint _tokenId, address _to, uint8 _amount) public onlyOwner {
+    require(exists(_tokenId), "That token does not exist!");
+
+    if (individualSupply[_tokenId] > 0) {
+      require(_amount <= balanceOf(_from, _tokenId), "Not enough tokens to send!");
+      _updateTokenBalance(msg.sender, _tokenId, _amount, ObjectLib.Operations.SUB);
+    }
+    
+    _updateTokenBalance(_to, _tokenId, _amount, ObjectLib.Operations.ADD);
+    emit TokenAwarded(_tokenId, _to, _amount);
   }
 }
