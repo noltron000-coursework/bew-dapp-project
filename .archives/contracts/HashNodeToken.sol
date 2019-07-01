260171a5eca02a90645de7a64fe6cadfa2787eab
Nolan Kovacik <noltron000@gmail.com>
2019-06-30 18:10:18 -0700

    deleting .archives/

--- .archives/contracts/HashNodeToken.sol
+++
@@ -1,9 +0,0 @@
-pragma solidity ^0.5.8;
-
-import 'openzeppelin-solidity/contracts/token/ERC20/ERC20Mintable.sol';
-
-contract HashnodeToken is ERC20Mintable {
-  string public name = "Hashnode Token";
-  string public symbol = "HT";
-  uint8 public decimals = 18;
-}
